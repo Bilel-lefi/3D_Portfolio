0e6c3439535c6ad02f512edc89d03d3e6276620f
bilel-lefi <bilellefi06@gmail.com>
2026-04-05 22:08:33 +0100

    v2

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -26,6 +26,7 @@ const ProjectCard = ({
           <img
             src={image}
             alt={name}
+            loading="lazy"
             className="w-full h-full object-cover group-hover:scale-105 transition-transform duration-500"
           />
           {/* Gradient overlay */}
